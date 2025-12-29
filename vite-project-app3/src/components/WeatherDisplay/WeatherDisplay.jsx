cd5405dc5ae0e8926259ca0be4f68c292afe4e12
gokulngk1 <gokulngk1@gmail.com>
2025-12-29 16:20:57 +0530

    card_changes

--- vite-project-app3/src/components/WeatherDisplay/WeatherDisplay.jsx
+++ vite-project-app3/src/components/WeatherDisplay/WeatherDisplay.jsx
@@ -14,12 +14,10 @@ const WeatherDisplay = ({ weather, loading }) => {
 
   return (
     <div className="weather-card">
+       <h2>{weather.name}, {weather.sys.country}</h2>
 
-     
-         <h2>
-        {weather.name}, {weather.sys.country}  
-        </h2>
-       <img
+       <div className="weathecard">
+        <img
         src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
         alt="weather"
       />
@@ -31,16 +29,20 @@ const WeatherDisplay = ({ weather, loading }) => {
           : `${Math.round(tempF)}°F`}
       </h1>
 
-      <button
+       </div>
+       
+
+      {/* <button
         className="unit-toggle"
         onClick={() => setUnit(unit === "C" ? "F" : "C")}
       >
         Switch to °{unit === "C" ? "F" : "C"}
-      </button>
+      </button> */}
 
-      <p className="desc">{weather.weather[0].description}</p>
+      
 
       <div className="col d-flex info">
+        <span>{weather.weather[0].description}</span>
         <span>Humidity: {weather.main.humidity}%</span>
         <span>Wind: {weather.wind.speed} km/h</span>
       </div>
@@ -51,44 +53,3 @@ const WeatherDisplay = ({ weather, loading }) => {
 export default WeatherDisplay;
 
 
-// import "./WeatherDisplay.css";
-// import { useUnit } from "../../context/UnitContext";
-
-// const WeatherDisplay = ({ weather, loading }) => {
-//   const { unit } = useUnit(); // ⬅️ only read unit
-
-//   if (loading) return null;
-//   if (!weather) return <p className="status">Search a city to view weather</p>;
-
-//   const tempC = weather.main.temp;
-//   const tempF = (tempC * 9) / 5 + 32;
-
-//   return (
-//     <div className="weather-card">
-//       <h2>
-//         {weather.name}, {weather.sys.country}
-//       </h2>
-
-//       <img
-//         src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-//         alt="weather"
-//       />
-
-//       {/* ✅ Temperature stays synced */}
-//       <h1>
-//         {unit === "C"
-//           ? `${Math.round(tempC)}°C`
-//           : `${Math.round(tempF)}°F`}
-//       </h1>
-
-//       <p className="desc">{weather.weather[0].description}</p>
-
-//       <div className="info">
-//         <span>Humidity: {weather.main.humidity}%</span>
-//         <span>Wind: {weather.wind.speed} km/h</span>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default WeatherDisplay;
